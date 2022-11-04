de2c6864468f210da00c7e661f1088f15b08a4ce
AndryZhidkih <johndoe@email.com>
2022-11-04 10:05:32 +0200

    Changed report and yml file

--- config/wdio.config.chrome.headless.js
+++ config/wdio.config.chrome.headless.js
@@ -6,6 +6,13 @@ const chromeHeadlessConfig = {
     capabilities: [{
         maxInstances: 5,
         browserName: "chrome",
+        'cjson:metadata': {
+            device: 'PC',
+            platform: {
+                name: 'Windows 11',
+                version: '10.0.22000'
+            }
+        },
         'goog:chromeOptions': {
             args: ['--headless', '--start-maximized', '--no-sandbox', '--disable-gpu', '--window-size=1280,800', '--allow-insecure-localhost']
         },
